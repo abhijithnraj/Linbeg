3704dfc221e4f76ea04a39556aff7285611dfa9f
abhijithnraj <abhijithnrajg@gmail.com>
2018-02-25 07:58:32 +0530

    completed 3 ques

--- 2nd cycle/2.sh
+++ 2nd cycle/2.sh
@@ -1,1 +1,29 @@
+function line(){
+echo "--------------------------------------------------------------------------"
+}
+line
+printf "The os version release no and kernel version is\n\n"
+printf "$(cat /proc/version)\n"
+line
 
+printf  "all available shells are \n\n"
+echo "$(cat /etc/shells)"
+#chsh -l is also correct
+line
+
+printf  "The cpu information is \n\n"
+echo "$(cat /proc/cpuinfo)"
+line
+
+printf "The memory information is \n\n"
+echo "$(cat /proc/meminfo )"
+line
+
+#printf "The hard disk info is \n\n"
+#echo "$(cat /proc/ide/hda/model)"
+#echo "$(cat /proc/ide/hda/driver)"
+#echo "$(cat /proc/ide/hda/cache)"
+
+printf "The mounted file system details are \n\n"
+echo "$(cat /proc/mounts)"
+line
